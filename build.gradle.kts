bea8c5b9754a7fa6b502e307348cace45e9484e8
someone5678 <59456192+someone5678@users.noreply.github.com>
2024-12-02 21:36:00 +0900

    custom-sdk: Initial configs for Lineage generateBp
    Change-Id: I15de36312046ca3ec5191204dd636d5e5cb4969b

---
+++ build.gradle.kts
@@ -0,0 +1,79 @@
+/*
+ * SPDX-FileCopyrightText: 2022-2024 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import org.lineageos.generatebp.GenerateBpPlugin
+import org.lineageos.generatebp.GenerateBpPluginExtension
+import org.lineageos.generatebp.models.Module
+
+plugins {
+    id("com.android.application") version "8.5.2"
+    id("org.jetbrains.kotlin.android") version "1.9.22"
+}
+
+apply {
+    plugin<GenerateBpPlugin>()
+}
+
+buildscript {
+    repositories {
+        maven("https://raw.githubusercontent.com/lineage-next/gradle-generatebp/v1.15/.m2")
+    }
+
+    dependencies {
+        classpath("org.lineageos:gradle-generatebp:+")
+    }
+}
+
+android {
+    compileSdk = 34
+    namespace = "org.parasite.customsdk"
+
+    defaultConfig {
+        applicationId = "org.parasite.customsdk"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    // Coil
+    implementation("io.coil-kt:coil:2.7.0")
+    implementation("io.coil-kt:coil-video:2.7.0")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:5.0.0-rc01")
+    implementation("com.github.bumptech.glide:annotations:5.0.0-rc01")
+    implementation("com.github.bumptech.glide:gifdecoder:5.0.0-rc01")
+    implementation("com.github.bumptech.glide:disklrucache:5.0.0-rc01")
+
+    // Android Hidden Api Bypass
+    implementation("org.lsposed.hiddenapibypass:hiddenapibypass:4.3")
+}
+
+configure<GenerateBpPluginExtension> {
+    targetSdk.set(android.defaultConfig.targetSdk!!)
+    availableInAOSP.set { module: Module ->
+        when {
+            module.group.startsWith("androidx") -> true
+            module.group.startsWith("org.jetbrains") -> true
+            module.group == "com.google.auto.value" -> true
+            module.group == "com.google.errorprone" -> true
+            module.group == "com.google.guava" -> true
+            module.group == "junit" -> true
+            else -> false
+        }
+    }
+}
